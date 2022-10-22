173a32f0f21eb0abbcf9bb3e2af3fd9608d3f52b
严欣他爹 <89913677+Alex-sunlight@users.noreply.github.com>
2022-10-22 10:01:46 +0800

    充值

--- pages/index/recharge.vue
+++ pages/index/recharge.vue
@@ -1,7 +1,11 @@
 <template>
 	<!-- U盾充值 -->
 	<view class="pages-recharge">
-		
+		<br>
+		<br>
+		<h3 style="width: 100%;text-align: left;margin-left: 20%;color: #fff;">充值金额</h3>
+		<br>
+		<input class="uni-input" focus v-model="email" />
 	</view>
 </template>
 
@@ -17,4 +21,13 @@
 		align-items: center;
 		background-color: #3972ab;
 	}
+	.uni-input {
+		width: 500rpx;
+		height: 60rpx;
+		color: #fff;
+		border: 2px solid #fff;
+		border-radius: 10px;
+	}
+
+	
 </style>
